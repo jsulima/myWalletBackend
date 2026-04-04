bc5804f7a722bba078864793f37a5a08cec8aa79
Julia Sulima <julia12.sulima@gmail.com>
2026-04-04 19:15:30 +0300

    refactor: remove all Prisma migration files and update docker-compose configuration

--- prisma/migrations/20260329165840_add_note_to_budget/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Budget" ADD COLUMN     "note" TEXT;
--- prisma/migrations/20260329183504_add_currency_to_financial_items/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE "Budget" ADD COLUMN     "currency" TEXT NOT NULL DEFAULT 'USD';
-
--- AlterTable
-ALTER TABLE "Credit" ADD COLUMN     "currency" TEXT NOT NULL DEFAULT 'USD';
-
--- AlterTable
-ALTER TABLE "SavingGoal" ADD COLUMN     "currency" TEXT NOT NULL DEFAULT 'USD';
